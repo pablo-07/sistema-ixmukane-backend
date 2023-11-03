edf6722c824b388e518c6bfbce51c3c127b3e891
Pablo <quiacainpablo.7@gmail.com>
2023-11-02 23:14:13 -0600

    corse

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "sistema-ixmukane-frontend-production.up.railway.app",
     credentials: true,
   })
 );
